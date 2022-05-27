0967b9ae4aad5eddccacc6f59efb1dac1df54f4c
scx567888 <scx567888@outlook.com>
2022-05-27 16:20:49 +0800

    1, 代码优化

--- demo/dark-mode/js/page-context.js
+++ demo/dark-mode/js/page-context.js
@@ -1,9 +1,12 @@
 class PageContext {
 
     themeSelect;
+    scxApp;
+    scxAppLoading;
+    scxAppLoadingStyle;
 
     eventMap = {
-        changeTheme: this.changeTheme.bind(this)
+        changeThemeFromSelect: this.changeThemeFromSelect.bind(this)
     }
 
     constructor() {
@@ -11,27 +14,52 @@ class PageContext {
     }
 
     initElement() {
-        this.themeSelect = document.querySelector("#themeSelect");
+        this.themeSelect = document.querySelector("#theme-select");
+        this.scxApp = document.querySelector("#scx-app");
+        this.scxAppLoading = document.querySelector("#scx-app-loading");
+        this.scxAppLoadingStyle = document.querySelector("#scx-app-loading-style");
         return this;
     }
 
     bindEvent() {
-        this.themeSelect.addEventListener("change", this.eventMap.changeTheme);
+        this.themeSelect.addEventListener("change", this.eventMap.changeThemeFromSelect);
         return this;
     }
 
     removeBindEvent() {
-        this.themeSelect.removeEventListener("change", this.eventMap.changeTheme);
+        this.themeSelect.removeEventListener("change", this.eventMap.changeThemeFromSelect);
         return this;
     }
 
-    changeTheme(e) {
+    loadThemeFromLocalStorage() {
+        let theme = localStorage.getItem("theme");
+        if (theme) {
+            theme = theme.trim();
+            this.themeSelect.value = theme;
+            this.changeTheme(theme);
+        }
+        return this;
+    }
+
+    showContent() {
+        document.body.removeChild(this.scxAppLoading);
+        document.head.removeChild(this.scxAppLoadingStyle);
+        this.scxApp.removeAttribute("style");
+        return this;
+    }
+
+    changeThemeFromSelect(e) {
         const val = e.target.value;
-        if (val === "dark") {
+        this.changeTheme(val);
+        localStorage.setItem("theme", val);
+    }
+
+    changeTheme(theme) {
+        if (theme === "dark") {
             document.documentElement.setAttribute('theme', "dark");
-        } else if (val === "light") {
+        } else if (theme === "light") {
             document.documentElement.setAttribute('theme', "light");
-        } else if (val === "system") {
+        } else if (theme === "system") {
             document.documentElement.removeAttribute('theme');
         }
     }
