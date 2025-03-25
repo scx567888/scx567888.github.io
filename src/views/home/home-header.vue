2bae6f7dd3ba70205d8d7e07965ab07f433aae0a
scx567888 <scx567888@outlook.com>
2025-03-25 20:20:20 +0800

    更新 Logo 地址

--- src/views/home/home-header.vue
+++ src/views/home/home-header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home-header">
         <div class="home-header-left">
-            <img alt="" src="https://scx.cool/logos/scx.svg">
+            <img alt="" src="https://scx.cool/scx-logo/scx.svg">
             欢迎来到 SCX
         </div>
         <div class="home-header-right">
@@ -13,7 +13,7 @@
                 <router-link :to="{name:'software-download-link'}">常用软件下载地址</router-link>
             </div>
             <div class="home-header-right-item"><a href="/docs/index.html">文档</a></div>
-            <div class="home-header-right-item"><a href="https://scx.cool/logos/">Logos</a></div>
+            <div class="home-header-right-item"><a href="https://scx.cool/scx-logo/">Logos</a></div>
         </div>
     </div>
 </template>
