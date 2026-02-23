2ec607ee7d8c73cae32ea5ba13af89571f5843fc
scx567888 <scx567888@outlook.com>
2026-02-23 12:24:52 +0800

    add blog (#23)

---
+++ src/views/blogs/2026/02/23/idea-plugin/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div style="padding: 20px">
+        <h3>IDEA 插件列表 (适用于 2025.3) 共 40 个</h3>
+        <textarea style="width: 80%;height: 200px;border: 1px solid red">
+classic-ui
+completionMlRanking
+css-plugin
+DatabaseTools
+debugger-collections-visualizer
+editorconfig-plugin
+findUsagesMl
+fullLine
+grid-plugin
+html-tools
+java
+java-byteCodeViewer
+java-coverage
+java-debugger-streams
+java-decompiler
+java-ide-customization
+javascript-debugger
+javascript-intentions
+javascript-plugin
+json
+jsonpath
+localization-zh
+markdown
+maven
+nodeJS
+platform-images
+postcss
+profiler-lineProfiler
+properties
+repository-search
+restClient
+searchEverywhereMl
+terminal
+ultimate-plugin
+vcs-git
+vcs-git-commit-modal
+vitejs
+vuejs-plugin
+webpack
+yaml
+</textarea>
+    </div>
+
+</template>
